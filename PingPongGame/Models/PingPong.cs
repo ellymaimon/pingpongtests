413ac17f70be2b5b73705856592887b42deb6045
Elly Maimon and Ryan Murry <elly@>
2018-06-26 15:51:33 -0700

    fix paramater and return logic

--- PingPongGame/Models/PingPong.cs
+++ PingPongGame/Models/PingPong.cs
@@ -6,46 +6,23 @@ namespace PingPongGame
     public class PingPong
     {
         private List<int> _rangeList = new List<int>();
+        private List<string> _finalList = new List<string>();
 
-        public bool IsNotInteger(string input)
+        public bool IsInteger(string input)
         {
             int num;
             bool result = int.TryParse(input, out num);
             return result;
         }
 
-        public bool FirstLastElement(int integer)
+        public List<int> PopulateRangeList(string input)
         {
+            int integer = int.Parse(input);
             for (int i = 1; i <= integer; i++)
             {
                 _rangeList.Add(i);
             }
-            
-            if (_rangeList[0] == 1 && _rangeList[integer - 1] == integer)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool LengthListTest(int integer)
-        {
-            for (int i = 1; i <= integer; i++)
-            {
-                _rangeList.Add(i);
-            }
-
-            if (_rangeList.Count == integer)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return _rangeList;
         }
 
         public string PingPongTest(int integer)
@@ -68,40 +45,15 @@ namespace PingPongGame
                 return intString;
             }
         }
-        // public bool PingTest(int integer)
-        // {
-        //     if (integer % 3 == 0)
-        //     {
-        //         return true;
-        //     }
-        //     else
-        //     {
-        //         return false;
-        //     }
-        // }
-        //
-        // public bool PongTest(int integer)
-        // {
-        //     if (integer % 5 == 0)
-        //     {
-        //         return true;
-        //     }
-        //     else
-        //     {
-        //         return false;
-        //     }
-        // }
-        //
-        // public bool BothTest(int integer)
-        // {
-        //     if (integer % 3 == 0 && integer % 5 == 0)
-        //     {
-        //         return true;
-        //     }
-        //     else
-        //     {
-        //         return false;
-        //     }
-        // }
+
+        public List<string> PopulateResultsList(string input)
+        {
+            _rangeList = PopulateRangeList(input);
+            for (int i = 0; i < _rangeList.Count; i++)
+            {
+                _finalList.Add(PingPongTest(_rangeList[i]));
+            }
+            return _finalList;
+        }
     }
 }
